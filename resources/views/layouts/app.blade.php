198d67ca0492e7fec91007117bcb469c2f990a17
jstoone <jstoone@drk.sh>
2020-07-27 20:51:20 +0200

    Update view layouts.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.base')
 
 @section('body')
-    @yield('content')
+    <div class="min-h-screen font-sans bg-gray-50">
+        <div class="container mx-auto max-w-lg p-6">
+            <h2 class="my-6 text-3xl font-extrabold text-center text-gray-900 leading-9">
+                DorkShare
+            </h2>
+
+            @yield('content')
+        </div>
+    </div>
 @endsection
